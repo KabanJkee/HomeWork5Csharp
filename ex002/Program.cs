d38ef0e7bac54785598ed70d9c659efb72230d31
KabanJkee <KabanJkee@gmail.com>
2022-07-13 15:45:56 +0300

    homework5

---
+++ ex002/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+int [] GetArray(int dimension)
+{
+int[] arr = new int[dimension];
+Random randomizer = new Random();
+
+for(int i=0; i < arr.Length; i++)
+{
+   arr[i] = randomizer.Next(0, 10);
+}
+
+return arr;
+}
+
+
+void PrintArray(int [] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+{
+   Console.Write($"{arr[i]} ");
+}
+}
+
+int Summary (int [] arr)
+{
+   int sum = 0;
+for (int i = 0; i < arr.Length; i = i + 2)
+{
+   sum = sum + arr[i];
+}
+return sum;
+}
+
+Console.WriteLine("Введите размерность массива:");
+int dimension = int.Parse(Console.ReadLine());
+
+Console.Clear();
+
+
+int [] arr = GetArray(dimension);
+PrintArray(arr);
+Console.WriteLine();
+
+int sum = Summary(arr);
+Console.WriteLine($"Сумма чисел стоящих на нечетных позициях в массиве = {sum}");
---
+++ ex003/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+
+*/
+Console.WriteLine("Введите размерность массива:");
+int dimension = int.Parse(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Сгенерированно {dimension} чисел в массиве:");
+Console.WriteLine();
+
+double[] arr = new double[dimension];
+  for (int i = 0; i < arr.Length; i++ )
+  {
+    arr[i] = new Random().NextDouble()*10;
+    Console.WriteLine($"{arr[i]:F2}  ");
+  }
+
+
+double max = arr[0];
+double min = arr[0];
+
+  for (int i = 1; i < arr.Length; i++)
+  {
+    if (max < arr[i])
+    {
+      max = arr[i];
+    }
+        if (min > arr[i])
+    {
+      min = arr[i];
+    }
+  }
+
+double decision = max - min;
+
+Console.WriteLine();
+Console.WriteLine($"Разница между между максимальным  и минимальным элементами: {max:F2} - {min:F2} = {decision:F2}");
+Console.WriteLine();
+Console.WriteLine();
